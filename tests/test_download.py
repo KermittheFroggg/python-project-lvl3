8aacdeb6462a876a13a10873bdb651598d86ea06
KermittheFroggg <santamantana@gmail.com>
2022-07-21 23:03:54 +0300

    step_6

--- tests/test_download.py
+++ tests/test_download.py
@@ -133,3 +133,61 @@ def test_errors():
             pook.get('https://ru.hexlet.io/courses', reply=400)
             download('https://ru.hexlet.io/courses', dir)
         assert e is not None
+
+
+def test_hexlet_1():
+    logger()
+    pook.on()
+    temp_dir = tempfile.TemporaryDirectory(dir=os.getcwd())
+    url =  'http://localhost/blog/about'
+    file_name = 'localhost-blog-about.html'
+    dir= os.path.join(os.getcwd(), temp_dir.name)
+    temp_path_file = os.path.join(dir, file_name)
+    shutil.copyfile('/home/frog/python-project-lvl3/tests/fixtures/localhost-blog-about.html', temp_path_file)
+    pook.get('http://localhost/blog/about/assets/styles.css', reply=200)
+    pook.get('http://localhost/photos/me.jpg', reply=200)
+    pook.get('http://localhost/assets/scripts.js', reply=200)
+    pook.get('http://localhost/blog/about.html', reply=200)
+    download_resources(temp_path_file,  url, dir)
+    ASSETS_DIR_NAME = 'localhost-blog-about_files'
+    assert len(os.listdir(dir)) == 2
+    assert len(os.listdir(os.path.join(dir, ASSETS_DIR_NAME))) == 4
+
+
+def test_hexlet_1_1():
+    logger()
+    pook.on()
+    temp_dir = tempfile.TemporaryDirectory(dir=os.getcwd())
+    url =  'https://site.com/blog/about'
+    file_name = 'site-com-blog-about.html'
+    dir= os.path.join(os.getcwd(), temp_dir.name)
+    temp_path_file = os.path.join(dir, file_name)
+
+    shutil.copyfile('/home/frog/python-project-lvl3/tests/fixtures/site-com-blog-about.html', temp_path_file)
+    pook.get('https://site.com/blog/about/assets/styles.css', reply=200)
+    pook.get('https://site.com/photos/me.jpg', reply=200)
+    pook.get('https://site.com/assets/scripts.js', reply=200)
+    pook.get('https://site.com/blog/about.html', reply=200)
+    download_resources(temp_path_file,  url, dir)
+    ASSETS_DIR_NAME = 'site-com-blog-about_files'
+    assert len(os.listdir(dir)) == 2
+    assert len(os.listdir(os.path.join(dir, ASSETS_DIR_NAME))) == 4
+
+
+def test_hexlet_2():
+    logger()
+    pook.on()
+    result_path = '/home/frog/python-project-lvl3/tests/fixtures/expected/site-com-blog-about.html'
+    with tempfile.NamedTemporaryFile() as tmpfilename:
+        shutil.copyfile('/home/frog/python-project-lvl3/tests/fixtures/site-com-blog-about.html', tmpfilename.name)
+        url =  'https://site.com/blog/about'
+        temp_path_file = tmpfilename.name
+        dir,_  = os.path.split(tmpfilename.name)
+        pook.get('https://site.com/blog/about/assets/styles.css', reply=200)
+        pook.get('https://site.com/photos/me.jpg', reply=200)
+        pook.get('https://site.com/assets/scripts.js', reply=200)
+        pook.get('https://site.com/blog/about.html', reply=200)
+        download_resources(temp_path_file,  url, dir)
+        with open(temp_path_file) as rslt:
+            with open(result_path) as result:
+                assert rslt.read() == result.read()
