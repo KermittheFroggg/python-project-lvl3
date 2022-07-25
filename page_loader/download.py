c6fafb23633c9169c349b95de4b3051f1a602785
KermittheFroggg <santamantana@gmail.com>
2022-07-25 23:49:33 +0300

    step_6

--- page_loader/download.py
+++ page_loader/download.py
@@ -8,19 +8,6 @@ from urllib.parse import urlparse
 from progress.bar import Bar
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.WARNING)
-stdout_handler.setFormatter(formatter)
-
-file_handler = logging.FileHandler('loader.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stdout_handler)
 
 
 def download(url, path):
