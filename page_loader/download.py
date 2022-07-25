cdddf09ac17850a52f1853f035580840317280b2
KermittheFroggg <santamantana@gmail.com>
2022-07-26 00:31:13 +0300

    step_6

--- page_loader/download.py
+++ page_loader/download.py
@@ -43,33 +43,48 @@ def download_resources(html_file_path, url, path):
         folder_with_resources = url_t_file_path(url) + '_files'
         if not os.path.exists(os.path.join(path, folder_with_resources)):
             os.mkdir(os.path.join(path, folder_with_resources))
-        with Bar('Downloading:', fill='░') as bar:
-            for img in all_img:
-                new_value_for_img = download_img(img, url, folder_with_resources, path)
-                if new_value_for_img != '':
-                    img['src'] = new_value_for_img
-                bar.next()
-        with Bar('Downloading:', fill='░') as bar:
-            for link in all_links:
-                new_value_for_link = download_link(link, url, folder_with_resources, path)
-                if new_value_for_link != '':
-                    link['href'] = new_value_for_link
-                bar.next()
-        with Bar('Downloading:', fill='░') as bar:
-            for script in all_scripts:
-                new_value_for_script = download_script(script, url, folder_with_resources, path)
-                if new_value_for_script != '':
-                    script['src'] = new_value_for_script
-                bar.next()
+        for_lint(all_img,
+                 all_links,
+                 all_scripts,
+                 url,
+                 folder_with_resources,
+                 path)
     with open(html_file_path, 'w') as fp:
         fp.write(soup.prettify())
+
+
+def for_lint(all_img, all_links, all_scripts, url, folder_with_resources, path):
+    with Bar('Downloading:', fill='░') as bar:
+        for img in all_img:
+            new_value_for_img = \
+                download_img(img, url, folder_with_resources, path)
+            if new_value_for_img != '':
+                img['src'] = new_value_for_img
+            bar.next()
+    with Bar('Downloading:', fill='░') as bar:
+        for link in all_links:
+            new_value_for_link = \
+                download_link(link, url, folder_with_resources, path)
+            if new_value_for_link != '':
+                link['href'] = new_value_for_link
+            bar.next()
+    with Bar('Downloading:', fill='░') as bar:
+        for script in all_scripts:
+            new_value_for_script = \
+                download_script(script, url, folder_with_resources, path)
+            if new_value_for_script != '':
+                script['src'] = new_value_for_script
+            bar.next()
 
 
 def download_img(img, url, folder_with_resources, path):
     if img.has_attr('src'):
         old_value_of_resource = img['src']
         new_value_of_resource = \
-            download_content(old_value_of_resource, url, folder_with_resources, path)
+            download_content(old_value_of_resource,
+                             url,
+                             folder_with_resources,
+                             path)
         return new_value_of_resource
 
 
@@ -77,7 +92,10 @@ def download_link(link, url, folder_with_resources, path):
     if link.has_attr('href'):
         old_value_of_resource = link['href']
         new_value_of_resource = \
-            download_content(old_value_of_resource, url, folder_with_resources, path)
+            download_content(old_value_of_resource,
+                             url,
+                             folder_with_resources,
+                             path)
         return new_value_of_resource
 
 
@@ -85,7 +103,10 @@ def download_script(script, url, folder_with_resources, path):
     if script.has_attr('src'):
         old_value_of_resource = script['src']
         new_value_of_resource = \
-            download_content(old_value_of_resource, url, folder_with_resources, path)
+            download_content(old_value_of_resource,
+                             url,
+                             folder_with_resources,
+                             path)
         return new_value_of_resource
 
 
@@ -102,7 +123,8 @@ def download_content(old_value_of_resource, url, folder_with_resources, path):
             raise requests.ConnectionError
         content = r.content
         folder_and_name_of_resource = (
-            os.path.join(folder_with_resources, url_t_file_path(url_without_ending))
+            os.path.join(folder_with_resources,
+                         url_t_file_path(url_without_ending))
         )
         resource_path_without_path = folder_and_name_of_resource + ending
         full_resource_path = os.path.join(path, resource_path_without_path)
@@ -117,17 +139,20 @@ def download_content(old_value_of_resource, url, folder_with_resources, path):
 def finding_scheme(old_value_of_resource, url):
     urlapass_old_value_of_resource = urlparse(old_value_of_resource)
     urlapass_url = urlparse(url)
-    if urlapass_old_value_of_resource.scheme != '' and urlapass_old_value_of_resource.netloc != '':
+    if urlapass_old_value_of_resource.scheme != '' \
+            and urlapass_old_value_of_resource.netloc != '':
         url_without_ending, ending = \
             diff_netloc(
                 urlapass_url.netloc, urlapass_old_value_of_resource.netloc,
                 old_value_of_resource
             )
-    elif urlapass_old_value_of_resource.scheme == '' and urlapass_old_value_of_resource.netloc == '':
+    elif urlapass_old_value_of_resource.scheme == '' and (
+            urlapass_old_value_of_resource.netloc == ''):
         url_without_ending, ending = os.path.splitext(old_value_of_resource)
         url_without_ending = urlapass_url.scheme + '://' + \
             urlapass_url.netloc + url_without_ending
-    elif urlapass_old_value_of_resource.scheme == '' and urlapass_old_value_of_resource.netloc != '':
+    elif urlapass_old_value_of_resource.scheme == '' and (
+            urlapass_old_value_of_resource.netloc != ''):
         if urlapass_url.netloc == urlapass_old_value_of_resource.netloc:
             url_without_ending, ending = os.path.splitext(old_value_of_resource)
             url_without_ending = urlapass_url.scheme + ':' + url_without_ending
@@ -144,12 +169,3 @@ def diff_netloc(urlapass_url_netloc, urlapass_netloc, src):
     else:
         url_without_ending, ending = None, None
     return url_without_ending, ending
-
-
-
-
-
-
-
-
-
